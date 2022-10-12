5964de3a2ad4c038b60912dcc0e6c8fa51a7f65f
董铭杰 <dongmj@getui.com>
2022-10-12 15:10:01 +0800

    [update]: 初始化

---
+++ examples/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        启动测试
+    </div>
+    <Map></Map>
+</template>
+<script lang="ts">
+import { Map } from 'whale-ui'
+import { defineComponent } from 'vue'
+export default defineComponent({
+  components: {
+    Map
+  }
+})
+</script>
---
+++ packages/components/src/map/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <button>测试按钮</button>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Map',
+  setup() {
+    
+  },
+})
+</script>
